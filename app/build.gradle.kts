6796f72c7dad0aea74115f20b89e1c4015bb56bc
Tanmay Kulkarni <tanmayk@Tanmays-Mac-mini.local>
2025-02-25 00:39:36 +0530

    Navigation added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
@@ -40,7 +41,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,11 +49,15 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.androidx.navigation.testing)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.compose
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.Spring
@@ -31,11 +32,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTopAppBarState
@@ -47,10 +47,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.example.compose.ui.theme.ComposeMasterTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,80 +69,46 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ComposeApp(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
-    Surface(modifier) {
-        if (shouldShowOnboarding) {
-            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-        } else {
-            //SmallAppbar()
-            //MediumAppBar()
-            val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
-            Scaffold (modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
-                ,topBar = {LargeTopAppBarCustom(scrollBehavior) }, bottomBar = { BottomAppBarCustom() }) {
-                innerPadding -> Greetings(modifier = Modifier.padding(innerPadding))
-            }
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = MainScreen) {
+        composable<MainScreen> {navBackStackEntry ->
+            val mainScreenArgs: MainScreen = navBackStackEntry.toRoute()
+            MainScreen(
+                modifier,
+                mainScreenArgs,
+                onListClicked = {
+                    navController.navigate(ListDetails(it))
+                }
+            )
+        }
+        composable<ListDetails> {
+            val listDetailsArgs = it.toRoute<ListDetails>()
+            ListDetailsScreen(modifier, ListDetails(listDetailsArgs.valuePassed), onBackClicked = {
+                navController.popBackStack()
+            })
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SmallAppbar() {
-    Scaffold(
-        topBar = {
-            TopAppBar(colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                titleContentColor = MaterialTheme.colorScheme.primary
-            ),
-                title = {
-                    Text("Small Top App Bar")
-                }
-            )
-        },
-    ) { innerPadding -> Greetings(modifier = Modifier.padding(innerPadding)) }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MediumAppBar() {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
-    Scaffold (
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            MediumTopAppBar(
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                titleContentColor = MaterialTheme.colorScheme.primary),
-            title = {
-                Text(
-                    "Medium Top App Bar",
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
-                )
-            },
-            navigationIcon = {
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = "Localized description"
-                    )
-                }
-            },
-            actions = {
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.Filled.Menu,
-                        contentDescription = "Localized description"
-                    )
-                }
-            },
-            scrollBehavior = scrollBehavior
-            )
-        },
-    ) {innerPadding -> Greetings(modifier = Modifier.padding(innerPadding)) }
+fun MainScreen(modifier: Modifier, mainScreenArgs: MainScreen, onListClicked: (number: String) -> Unit) {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    var onboardingName by rememberSaveable {  mutableStateOf("") }
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false }, name = onboardingName, onNameChange = {onboardingName = it})
+        } else {
+            val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+            Scaffold (modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+                ,topBar = {LargeTopAppBarCustom(scrollBehavior) }, bottomBar = { BottomAppBarCustom() }) {
+                    innerPadding -> Greetings(modifier = Modifier.padding(innerPadding), onListClicked=onListClicked)
+            }
+            ShowToast(onboardingName)
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -210,69 +182,72 @@ fun BottomAppBarCustom() {
     )
 }
 
-//Need to check on this, unresolved parameters
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//fun CenterAlignedTopAppBar() {
-//    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
-//
-//    Scaffold(
-//        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-//        topBar = {
-//            CenterAlignedTopAppBar(
-//                title = { Text("Centered TopAppBar") },
-//                navigationIcon = {
-//                    IconButton(onClick = { /* doSomething() */ }) {
-//                        Icon(
-//                            imageVector = Icons.Filled.Menu,
-//                            contentDescription = "Localized description"
-//                        )
-//                    }
-//                }
-//            )
-//        },
-//    ) { innerPadding -> Greetings(modifier = Modifier.padding(innerPadding))}
-//}
-
 @Composable
-fun OnboardingScreen(
-    onContinueClicked: () -> Unit,
-    modifier: Modifier = Modifier
-) {
+fun OnboardingContent(modifier: Modifier, onContinueClicked: () -> Unit, name: String, onNameChange: (String) -> Unit) {
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Welcome to the compose master app!")
+        OnBoardingNameInput(name, onNameChange = onNameChange)
         Button(
-            modifier = Modifier.padding(vertical = 24.dp),
-            onClick = onContinueClicked
+            modifier = modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked,
+            enabled = name.isNotBlank()
         ) {
             Text("Continue")
         }
     }
+}
 
+//State Hoisting the Input component
+@Composable
+fun OnBoardingNameInput(name: String, onNameChange:(String) -> Unit) {
+    OutlinedTextField(
+        value = name,
+        onValueChange = onNameChange,
+        label = { Text("Enter your name") },
+        modifier = Modifier.padding(vertical = 24.dp)
+    )
+}
+
+@Composable
+fun OnboardingScreen(
+    modifier: Modifier = Modifier,
+    onContinueClicked: () -> Unit,
+    name: String,
+    onNameChange: (String) -> Unit
+) {
+    OnboardingContent(modifier = modifier, onContinueClicked, name =name, onNameChange = onNameChange)
 }
 
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = List(100) { "$it" }
+    numbers: List<String> = List(100) { "$it" },
+    onListClicked: (number: String) -> Unit
 ) {
-    var selectedName by rememberSaveable { mutableStateOf<String>("") }
+    var selectedNumber by rememberSaveable { mutableStateOf<String>("") }
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
-        items(items = names) { name ->
-            Greeting(name = name, isSelected = (selectedName == name),
-                onNameSelected = { selected ->
-                    selectedName = if (selectedName == selected) "" else selected
-                })
+        items(items = numbers) { number ->
+            Greeting(
+                number = number, isSelected = (selectedNumber == number),
+                onShowClicked = { selected ->
+                    selectedNumber = if (selectedNumber == selected) "" else selected
+                },
+                onListClicked = onListClicked)
         }
     }
 }
 
 @Composable
-private fun Greeting(name: String, isSelected: Boolean, onNameSelected: (name: String) -> Unit ,modifier: Modifier = Modifier) {
+private fun Greeting(
+    modifier: Modifier = Modifier,
+    number: String, isSelected: Boolean,
+    onShowClicked: (name: String) -> Unit,
+    onListClicked: (number: String) -> Unit
+) {
 
     val extraPadding by animateDpAsState(
         if (isSelected) 48.dp else 0.dp,
@@ -284,21 +259,23 @@ private fun Greeting(name: String, isSelected: Boolean, onNameSelected: (name: S
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+        onClick = {onListClicked(number)}
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = number)
                 if (isSelected) {
-                    Text(text = "This is additional detail about $name")
+                    Text(text = "This is additional detail about $number")
                 }
             }
             ElevatedButton(
-                onClick = {onNameSelected(name) }
+                onClick = { onShowClicked(number) }
             ) {
                 Text(if (isSelected) "Show less" else "Show more")
             }
@@ -306,12 +283,37 @@ private fun Greeting(name: String, isSelected: Boolean, onNameSelected: (name: S
     }
 }
 
-//Previews
+@Composable
+fun ShowToast(name: String) {
+    val context = LocalContext.current
+    Toast.makeText(context, "Hello! $name", Toast.LENGTH_LONG).show()
+}
+
+@Composable
+fun ListDetailsScreen(modifier: Modifier, listDetails: ListDetails, onBackClicked: () -> Unit) {
+    Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text("Hello, ${listDetails.valuePassed}")
+        Button(onClick = onBackClicked) {
+            Text("Back")
+        }
+    }
+}
+
+//Data class that holds argument to be passed to route
+@Serializable
+data class ListDetails(val valuePassed: String)
+
+//object to have main screen as the start screen in the route
+@Serializable
+object MainScreen
+
+/*Previews*/
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun OnboardingPreview() {
+fun OnBoardingScreenPreview() {
     ComposeMasterTheme  {
-        OnboardingScreen(onContinueClicked = {})
+        OnboardingScreen(modifier = Modifier, onContinueClicked = {}, name = "", onNameChange = {})
     }
 }
 
@@ -322,7 +324,7 @@ fun OnboardingPreview() {
 @Composable
 fun GreetingPreview() {
     ComposeMasterTheme {
-        Greetings()
+        Greetings(onListClicked = {})
     }
 }
 
@@ -331,5 +333,13 @@ fun GreetingPreview() {
 fun ComposeAppPreview() {
     ComposeMasterTheme  {
         ComposeApp(Modifier.fillMaxSize())
+    }
+}
+
+@Preview
+@Composable
+fun ListDetailsScreenPreview() {
+    ComposeMasterTheme {
+        ListDetailsScreen(Modifier.fillMaxSize(), ListDetails("20"), onBackClicked = {})
     }
 }
